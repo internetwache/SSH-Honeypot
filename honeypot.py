fc60f3fc23d5f88573a3cb6eeb34f0b6c96fb661
think <60181010+josemlwdf@users.noreply.github.com>
2023-05-08 11:15:44 +0200

    Python 3 update
    I just changed the script to be used with python 3 since is the one i used. :)

--- honeypot.py
+++ honeypot.py
@@ -1,34 +1,34 @@
-#!/usr/bin/env python2.7
-import socket, sys, threading, thread
+#!/usr/bin/env python3
+import socket, sys, threading
+import _thread as thread
 import paramiko
 
-#generate keys with 'ssh-keygen -t rsa -f server.key'
+# generate keys with 'ssh-keygen -t rsa -f server.key'
 HOST_KEY = paramiko.RSAKey(filename='server.key')
 SSH_PORT = 2222
-LOGFILE = 'logins.txt' #File to log the user:password combinations to
+LOGFILE = 'logins.txt' # File to log the user:password combinations to
 LOGFILE_LOCK = threading.Lock()
 
-class SSHServerHandler (paramiko.ServerInterface):
+class SSHServerHandler(paramiko.ServerInterface):
     def __init__(self):
         self.event = threading.Event()
 
     def check_auth_password(self, username, password):
         LOGFILE_LOCK.acquire()
         try:
-            logfile_handle = open(LOGFILE,"a")
-            print("New login: " + username + ":" + password)
-            logfile_handle.write(username + ":" + password + "\n")
-            logfile_handle.close()
+            with open(LOGFILE, "a") as logfile_handle:
+                print("New login: " + username + ":" + password)
+                logfile_handle.write(username + ":" + password + "\n")
         finally:
             LOGFILE_LOCK.release()
         return paramiko.AUTH_FAILED
-
 
     def get_allowed_auths(self, username):
         return 'password'
 
 def handleConnection(client):
     transport = paramiko.Transport(client)
+    
     transport.add_server_key(HOST_KEY)
 
     server_handler = SSHServerHandler()
@@ -38,6 +38,7 @@ def handleConnection(client):
     channel = transport.accept(1)
     if not channel is None:
         channel.close()
+        print('Closing connection.')
 
 def main():
     try:
@@ -45,13 +46,15 @@ def main():
         server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         server_socket.bind(('', SSH_PORT))
         server_socket.listen(100)
+        print('SSH Honeypot Server Started.')
 
-        paramiko.util.log_to_file ('paramiko.log') 
+        paramiko.util.log_to_file('paramiko.log')
 
-        while(True):
+        while True:
             try:
                 client_socket, client_addr = server_socket.accept()
-                thread.start_new_thread(handleConnection,(client_socket,))
+                print('Connection Received From:', client_addr)
+                thread.start_new_thread(handleConnection, (client_socket,))
             except Exception as e:
                 print("ERROR: Client handling")
                 print(e)
